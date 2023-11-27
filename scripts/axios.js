972cf8a8f0288c4481a99ed2f4fd388506b3cc70
Kiofu7w7 <buitragogiraldosantiago@gmail.com>
2023-11-27 15:18:39 -0500

    creacion y finalizacion de funciones con AXIOS

---
+++ scripts/axios.js
@@ -0,0 +1,64 @@
+const url = 'http://localhost:3000/productos'
+
+//--------------------Obtener productos-------------------//
+const obtenerProductos = async () => {
+    try {
+        const response = await axios.get(url)
+        return response
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//--------------------Buscar producto-------------------//
+const buscarProducto = async (idB) => {
+    try {
+        const response = await axios.get(url + "/" + idB)
+        return response
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//--------------------Crear productos-------------------//
+const crearProducto = async (nombreB, precioB, imagenB, descB, catB) => {
+    try {
+        await axios.post(url, {
+            id: crypto.randomUUID(),
+            nombreProducto: nombreB,
+            precio: precioB,
+            imagen: imagenB,
+            descripcion: descB,
+            categoria: catB
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//--------------------Borrar producto-------------------//
+
+const borrarProducto = async (id) => {
+    try {
+        await axios.delete(url+"/"+id)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+//--------------------Editar producto-------------------//
+
+const editarProducto = async (id,nombreB, precioB, imagenB, descB, catB) => {
+    try {
+        await axios.put(url+"/"+id,{
+            id: id,
+            nombreProducto: nombreB,
+            precio: precioB,
+            imagen: imagenB,
+            descripcion: descB,
+            categoria: catB
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
