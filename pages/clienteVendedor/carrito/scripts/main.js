051d8e1ac7c07a9e6b37df160d9a1c64f0d4e025
Kiofu7w7 <buitragogiraldosantiago@gmail.com>
2023-11-29 18:27:16 -0500

    cambios

--- pages/clienteVendedor/carrito/scripts/main.js
+++ pages/clienteVendedor/carrito/scripts/main.js
@@ -5,32 +5,34 @@ import { buscarProducto } from "../../../../scripts/axiosFunc/axiosProducto.js"
 
 const idUsuario = "1" ///ESTO SE CAMBIA CUANDO SE TENGA EL LOCAL STORAGE
 const prueba = await buscarCarrito(idUsuario)
-const obtenePrecios = Object.keys(prueba.productos) //SE OBTIENEN TODAS LAS IDS DEL CARRITO
-const contenedor = document.getElementById("container")
-const nombreHtml = document.getElementById("nombreHTML")
-const contactoHtml = document.getElementById("contactoHTML")
-const direccionHtml = document.getElementById("direccionHTML")
-const totalHtml = document.getElementById("totalHTML")
-const precios = []
-const usuario = await buscarUsuario(idUsuario)
-let total = 0
+console.log()
 
-for (let i = 0; i < obtenePrecios.length; i++) {
-    const productoPre = await buscarProducto(obtenePrecios[i])
-    precios.push(productoPre.data.precio)
-}
+    const obtenePrecios = Object.keys(prueba.productos) //SE OBTIENEN TODAS LAS IDS DEL CARRITO
+    const contenedor = document.getElementById("container")
+    const nombreHtml = document.getElementById("nombreHTML")
+    const contactoHtml = document.getElementById("contactoHTML")
+    const direccionHtml = document.getElementById("direccionHTML")
+    const totalHtml = document.getElementById("totalHTML")
+    const precios = []
+    const usuario = await buscarUsuario(idUsuario)
+    let total = 0
 
-precios.forEach(element => {
-    total += parseInt(element)
-});
+    for (let i = 0; i < obtenePrecios.length; i++) {
+        const productoPre = await buscarProducto(obtenePrecios[i])
+        precios.push(productoPre.data.precio)
+    }
 
-nombreHtml.innerText += " " + usuario.data.primerNombre + " " + usuario.data.primerApellido
-contactoHtml.innerText += " " + usuario.data.numeroContacto
-direccionHtml.innerText += " " + usuario.data.direccion
-totalHtml.innerText += " " + total
+    precios.forEach(element => {
+        total += parseInt(element)
+    });
 
-if (prueba == "false") {
-    console.log("No cuenta con carrito")
-} else {
-    viewCarritoProductos(prueba, contenedor)
-}
+    nombreHtml.innerText += " " + usuario.data.primerNombre + " " + usuario.data.primerApellido
+    contactoHtml.innerText += " " + usuario.data.numeroContacto
+    direccionHtml.innerText += " " + usuario.data.direccion
+    totalHtml.innerText += " " + total
+
+    if (prueba == "false") {
+        console.log("No cuenta con carrito")
+    } else {
+        viewCarritoProductos(prueba, contenedor)
+    }    
--- pages/clienteVendedor/login/scripts/main.js
+++ pages/clienteVendedor/login/scripts/main.js
@@ -6,6 +6,7 @@ async function ejecutarInicioSesion(emailEnviar, contraEnviar) {
             const variable = await buscarUsuario(emailEnviar, contraEnviar);
             if (variable == true){
                 window.location="../../clienteVendedor/landing/index.html ";
+                
             }else{
                 alert("Datos no coinciden con la base de datos");
             }
