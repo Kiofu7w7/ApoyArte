f563502b6a321bb3a50053bf9812c7401c12a51e
Kiofu7w7 <buitragogiraldosantiago@gmail.com>
2023-11-30 00:01:47 -0500

    contacto, blog y funcionalidad navbar carrito y ajustes

---
+++ pages/navBarFotter/scripts/main.js
@@ -0,0 +1,15 @@
+import { buscarCarrito } from "../../../../scripts/axiosFunc/axiosCarritos.js"
+
+const idUsuario = "1" ///ESTO SE CAMBIA CUANDO SE TENGA EL LOCAL STORAGE
+const carrito = await buscarCarrito(idUsuario)
+const obtenePrecios = Object.keys(carrito.productos)
+console.log(obtenePrecios.length)
+const numero = obtenePrecios.length
+const numeroCarrito = document.getElementById("numeroCarrito")
+
+if(numero!=0){
+    numeroCarrito.style.display = ""
+    numeroCarrito.innerText = numero
+}else{
+    numeroCarrito.style.display = "none"
+}
